edb6081f6eee0c926b446c8b1e7a1a3abe26b104
Sebastian Schneider <Sebastian.Schneider@assecor.de>
2020-11-13 11:16:42 +0100

    init repo

---
+++ src/main/java/com/finger/uebung/UebungApplication.java
@@ -0,0 +1,13 @@
+package com.finger.uebung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UebungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finger/uebung/UebungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finger.uebung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
